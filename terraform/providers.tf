63271c1f07ca0ed9ef7bb62bfdd599475373c1f0
ram <ramki.nv@gmail.com>
2025-12-29 22:00:37 +0530

    Add Jenkins CI/CD pipeline15

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
   # Use the Managed Identity of the Jenkins VM
   use_msi         = true
 
-  # Keep your subscription variable if you want
+  # Keep your subscriptions variable if you want
   subscription_id = var.azure_subscription_id
 }
