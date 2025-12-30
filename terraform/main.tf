cff037927a90fcd82ae3e4ae40d56b1b30b56437
ram <ramki.nv@gmail.com>
2025-12-30 12:11:09 +0530

    Add Jenkins CI/CD pipeline19

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,3 +37,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
     network_plugin = "azure"
   }
 }
+
+###########################################################
+# Give AKS kubelet permission to pull from ACR
+###########################################################
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  depends_on           = [azurerm_kubernetes_cluster.aks]
+}
