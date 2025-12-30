4fc81abaf625b21b086706a127b7e65c39a43236
ram <ramki.nv@gmail.com>
2025-12-30 16:06:46 +0530

    Add Jenkins CI/CD pipeline19

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "sre-demo-rg"
+    storage_account_name = "sretfstateacct123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
